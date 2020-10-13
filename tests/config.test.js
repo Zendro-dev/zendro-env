49db05fc605f349467fa3a70d3153546f39689dd
David Velasco <davvelsan@gmail.com>
2020-10-13 11:53:12 +0200

    tests: migrate from jest to mocha/chai/sinon/nyc

--- tests/config.test.js
+++ tests/config.test.js
@@ -1,22 +1,135 @@
-const { getConfig }      = require('../src/config/config');
-const { checkWorkspace } = require('../src/config/helpers');
+const { join } = require('path');
+
+const { expect, use }            = require('chai');
+const { describe, before, test } = require('mocha');
+const { replace, restore }       = require('sinon');
+
+use( require('chai-as-promised') );
 
 
-describe('App config', () => {
+describe('Tool configuration', () => {
 
-  // beforeAll(async () => {
-  //   const { mkdir } = require('fs/promises');
+  let configObj;
 
-  //   await mkdir()
-  // });
+  before(() => {
 
-  test('01. should find the .testenv.json file', () => {
+    /**
+     * Replace the process.cwd() call to return the absolute path to the
+     * mocks folder.
+     *
+     * This is required for the `find-up` module to find the config
+     * file localted in mocks/.
+     */
 
-    const configJson = require('./mocks/.testenv.json');
-    const configObj  = getConfig();
+    const cwd = process.cwd();
+    replace(process, 'cwd', () => join(cwd, './tests/mocks'));
 
-    expect(configObj).toEqual(configJson);
+    const { getConfig } = require('../src/config/config');
+    configObj = getConfig();
 
+  });
+
+  describe('Config is imported', () => {
+
+    test('01. should find the .testenv.json file', () => {
+
+      const configJson = require('./mocks/.testenv.json');
+      expect(configObj).to.deep.equal(configJson);
+
+    });
+
+  });
+
+  describe('Helper `checkWorkspace` is invoked', () => {
+
+    before(function () {
+
+      /**
+       * Mock the `stat` async function to resolve or reject the expected paths
+       * as requested by the `checkWorkspace` function.
+       *
+       * Here we mock some folders as being present or absent (ENOENT) within the workspace.
+       * An illegal exception is also thrown to test other errors than ENOENT.
+       */
+
+      replace(require('fs/promises'), 'stat', (path) => {
+
+        const exist = [
+          configObj.cwd,
+          join(configObj.cwd, 'templates'),
+        ];
+
+        const notexist = [
+          join(configObj.cwd, 'services'),
+          join(configObj.cwd, 'node_modules'),
+        ];
+
+        if (exist.includes(path))
+          return Promise.resolve(true);
+
+        if (notexist.includes(path))
+          return Promise.reject({ code: 'ENOENT'});
+
+        throw new Error('Non-ENOENT error thrown');
+      });
+
+    });
+
+    test('02. should correctly check the workspace integrity', async () => {
+
+      const { checkWorkspace } = require('../src/config/helpers');
+      const exists = await checkWorkspace(configObj.cwd);
+
+      expect(exists).to.deep.equal({
+        modules: false,
+        services: false,
+        templates: true,
+        workspace: true,
+      });
+
+    });
+
+    test('03. should throw on unexpected error', async () => {
+
+      const { checkWorkspace } = require('../src/config/helpers');
+      expect(checkWorkspace('illegal error')).to.be.rejectedWith(Error);
+
+    });
+
+  });
+
+  describe('Helper `expandPath` is invoked', () => {
+
+    test('04. should correctly expand service and template paths', () => {
+
+      const { expandPath } = require('../src/config/helpers');
+
+      const servicePath  = expandPath('gql_server1');
+      const templatePath = expandPath('gql');
+
+      expect(servicePath).to.equal('services/gql_server1');
+      expect(templatePath).to.equal('templates/gql');
+
+    });
+
+    test('05. should return null when a service or template name does not exist', () => {
+
+      const { expandPath } = require('../src/config/helpers');
+
+      const missingPath = expandPath('unknown-name');
+
+      expect(missingPath).to.be.null;
+
+    });
+  });
+
+  after(function () {
+
+    /**
+     * Use sinon.restore() to undo all the replaced functions used within the
+     * tests.
+     */
+    restore();
   });
 
 });
