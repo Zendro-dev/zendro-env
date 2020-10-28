e5e3a0504a83d9c730af792a89f0aaae5ebc6827
David Velasco <davvelsan@gmail.com>
2020-10-28 21:02:45 +0100

    feat: remove find-up functionality feat: support default cwd

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,56 @@
-const { commandSync }  = require('execa');
-const { sync }         = require('find-up');
-const { readFileSync } = require('fs');
-const { platform }     = require('os');
-const { cwd }          = require('process');
-//
+const { commandSync }            = require('execa');
+const { readFileSync, statSync } = require('fs');
+const { platform }               = require('os');
+const { join }                   = require('path');
+const { cwd }                    = require('process');
+
 require('./typings');
 
 
+/* CONFIGURATION */
+
 // Get config file path, if it exists
-const configPath = sync(['.testenv.json', '.testenvrc'], {
-  cwd: cwd()
-});
+const configNames = ['.testenv.json', '.testenvrc'];
+let configPath;
+
+for (const name of configNames) {
+
+  try {
+    configPath = join(cwd(), name);
+    statSync(configPath);
+  }
+  catch (error) {
+    if (error.code !== 'ENOENT')
+      throw error;
+    configPath = null;
+  }
+
+  if (configPath)
+    break;
+}
 
 if (!configPath) throw new Error(
   'To use the zendro-env CLI, a properly configured ".testenvrc" or' +
   ' ".testenv.json" file is required in the project folder'
 );
+console.log(`\nLoaded configuration: "${configPath}"`);
 
-//  config JSON object
+// Read config file and set defaults
+/** @type {EnvConfig} */
 const config = JSON.parse(
   readFileSync(configPath, { encoding: 'utf-8'})
 );
 
+if (!config.cwd) {
+  config.cwd = cwd();
+}
+
+if (!config.env) {
+  config.env = {};
+}
+
+
+/* ENVIRONMENT */
 
 let runtimeEnv = {};
 const os = platform();
@@ -30,7 +59,9 @@ try {
   const userId  = commandSync('id -u').stdout;
   const groupId = commandSync('id -g').stdout;
   runtimeEnv.UID = `${userId}:${groupId}`;
-  console.log(`Starting zendro-env in ${os} as user:group ${userId}:${groupId}`);
+  console.log(
+    `Starting zendro-env in ${os} as user:group ${userId}:${groupId}`
+  );
 }
 catch (error) {
   console.warn(
@@ -45,6 +76,8 @@ process.env = {
   ...runtimeEnv,
 };
 
+
+/* AUXILIARY FUNCTIONS */
 
 /**
  * Get workspace configuration.
